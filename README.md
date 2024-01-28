7c4abd0f2fdbc642d714cf67934d68cc974f69f1
Satoshi Imai <satoshi.imai@finatext.com>
2024-01-28 17:17:58 +0900

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # groupbychunkreader
+
 A group by chunk reading wrapper for Pandas DataFrame.
+
+## GroupByChunkReader
+
+An iterator wrapper for Pandas DataFrame based on a control-break algorithm.
+It expects chunked pandas dataframe which is returned from `read_csv` or `read_sql` with `chunksize` parameter.
+It supports only one key column. So you need merge multiple keys into one key column.
+The key column has to be sorted BEFORE pandas read. It is expected to be sorted by SQL level or pre-process for csv.
+It pools at least 2 keys' dataframe. So you need to manage break key size to be able to load at once 2 keys into a memory. And you need to manage pandas read `chunksize` to be able to load into a memory, too.
+
+```python
+import pandas as pd
+from groupbychunkreader import GroupByChunkReader
+
+type_def = {'value': int, 'key': 'object'}
+
+# test data
+test_csv_df = pd.DataFrame([[x, '01'] for x in range(10)] + 
+                           [[x, '02'] for x in range(20)], 
+                           columns=list(type_def))
+test_csv_df.to_csv('...data path')
+
+# load by the grouped chunk
+test_csv_df = pd.read_csv(
+    '...data path',
+    dtype=type_def, chunksize=6)
+
+my_reader = GroupByChunkReader(test_csv_df, 'key')
+count = 0
+for this_df in my_reader:
+    # do operation by group
+```
 
 ## LICENSE
 
--- tests/test_groupbychunkreader.py
+++ tests/test_groupbychunkreader.py
@@ -6,6 +6,7 @@
 # ---------------------------------------------------------------------------
 
 import logging
+import random
 import shutil
 import tempfile
 from logging import Logger, StreamHandler
@@ -18,7 +19,6 @@ import pytest
 from src.groupbychunkreader import GroupByChunkReader
 
 test_csv_format = 'test_csv_{0:0>3}.csv'
-columns = ['value', 'key']
 chunksize = 3
 type_def = {'value': int, 'key': 'object'}
 
@@ -67,22 +67,22 @@ def test_key1_patterns(tempdir: Path, logger: Logger):
 
     # key1 chunk<1
     test_csv_01_df = pd.DataFrame([[x, '01']
-                                  for x in range(chunksize - 1)], columns=columns)
+                                  for x in range(chunksize - 1)], columns=list(type_def))
     test_csv_01_df.to_csv(tempdir.joinpath(test_csv_format.format(1)))
 
     # key1 chunk=1
     test_csv_02_df = pd.DataFrame([[x, '01']
-                                  for x in range(chunksize)], columns=columns)
+                                  for x in range(chunksize)], columns=list(type_def))
     test_csv_02_df.to_csv(tempdir.joinpath(test_csv_format.format(2)))
 
     # key1 chunk>1
     test_csv_03_df = pd.DataFrame([[x, '01']
-                                  for x in range(chunksize * 2 - 1)], columns=columns)
+                                  for x in range(chunksize * 2 - 1)], columns=list(type_def))
     test_csv_03_df.to_csv(tempdir.joinpath(test_csv_format.format(3)))
 
     # key1 chunk>2
     test_csv_04_df = pd.DataFrame([[x, '01']
-                                  for x in range(chunksize * 3 + 1)], columns=columns)
+                                  for x in range(chunksize * 3 + 1)], columns=list(type_def))
     test_csv_04_df.to_csv(tempdir.joinpath(test_csv_format.format(4)))
 
     # key1 chunk<1
@@ -153,6 +153,348 @@ def test_key1_patterns(tempdir: Path, logger: Logger):
         # end for
 
     assert count == 1
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+    # end def
+
+
+@pytest.mark.run(order=20)
+def test_key2_patterns(tempdir: Path, logger: Logger):
+    logger.info('key2_patterns')
+
+    # key2 chunk<1
+    test_csv_01_df = pd.DataFrame([[x, '01'] for x in range(
+        chunksize - 1)] + [[x, '02'] for x in range(chunksize - 1)], columns=list(type_def))
+    test_csv_01_df.to_csv(tempdir.joinpath(test_csv_format.format(11)))
+
+    # key2 chunk=1
+    test_csv_02_df = pd.DataFrame([[x, '01'] for x in range(
+        chunksize)] + [[x, '02'] for x in range(chunksize)], columns=list(type_def))
+    test_csv_02_df.to_csv(tempdir.joinpath(test_csv_format.format(12)))
+
+    # key2 chunk>1
+    test_csv_03_df = pd.DataFrame([[x,
+                                    '01'] for x in range(chunksize * 2 - 1)] + [[x,
+                                                                                 '02'] for x in range(chunksize * 2 - 1)],
+                                  columns=list(type_def))
+    test_csv_03_df.to_csv(tempdir.joinpath(test_csv_format.format(13)))
+
+    # key2 chunk>2
+    test_csv_04_df = pd.DataFrame([[x,
+                                    '01'] for x in range(chunksize * 3 + 1)] + [[x,
+                                                                                 '02'] for x in range(chunksize * 3 + 1)],
+                                  columns=list(type_def))
+    test_csv_04_df.to_csv(tempdir.joinpath(test_csv_format.format(14)))
+
+    # key2 chunk<1
+    test_csv_01_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(11)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_01_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize - 1
+        # end for
+
+    assert count == 2
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key2 chunk=1
+    test_csv_02_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(12)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_02_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize
+        # end for
+
+    assert count == 2
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key2 chunk>1
+    test_csv_03_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(13)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_03_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize * 2 - 1
+        # end for
+
+    assert count == 2
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key2 chunk>2
+    test_csv_04_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(14)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_04_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize * 3 + 1
+        # end for
+
+    assert count == 2
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key1,2 chunk<1
+    test_csv_05_df = pd.DataFrame([[x, '01'] for x in range(
+        chunksize - 2)] + [[x, '02'] for x in range(chunksize - 2)], columns=list(type_def))
+    test_csv_05_df.to_csv(tempdir.joinpath(test_csv_format.format(15)))
+
+    test_csv_05_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(15)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_05_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize - 2
+        # end for
+
+    assert count == 2
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key1 chunk<1, key2 chunk=1
+    test_csv_06_df = pd.DataFrame([[x, '01'] for x in range(
+        chunksize - 2)] + [[x, '02'] for x in range(chunksize)], columns=list(type_def))
+    test_csv_06_df.to_csv(tempdir.joinpath(test_csv_format.format(16)))
+
+    test_csv_06_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(16)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_06_df, 'key')
+    count = 0
+    expected = {1: chunksize - 2, 2: chunksize}
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == expected[count]
+        # end for
+
+    assert count == 2
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+    # end def
+
+
+@pytest.mark.run(order=30)
+def test_key3_patterns(tempdir: Path, logger: Logger):
+    logger.info('key2_patterns')
+
+    # key2 chunk<1
+    test_csv_01_df = pd.DataFrame([[x, '01'] for x in range(chunksize - 1)] +
+                                  [[x, '02'] for x in range(chunksize - 1)] +
+                                  [[x, '03'] for x in range(chunksize - 1)],
+                                  columns=list(type_def))
+    test_csv_01_df.to_csv(tempdir.joinpath(test_csv_format.format(21)))
+
+    # key2 chunk=1
+    test_csv_02_df = pd.DataFrame([[x, '01'] for x in range(chunksize)] +
+                                  [[x, '02'] for x in range(chunksize)] +
+                                  [[x, '03'] for x in range(chunksize)],
+                                  columns=list(type_def))
+    test_csv_02_df.to_csv(tempdir.joinpath(test_csv_format.format(22)))
+
+    # key2 chunk>1
+    test_csv_03_df = pd.DataFrame([[x, '01'] for x in range(chunksize * 2 - 1)] +
+                                  [[x, '02'] for x in range(chunksize * 2 - 1)] +
+                                  [[x, '03']
+                                      for x in range(chunksize * 2 - 1)],
+                                  columns=list(type_def))
+    test_csv_03_df.to_csv(tempdir.joinpath(test_csv_format.format(23)))
+
+    # key2 chunk>2
+    test_csv_04_df = pd.DataFrame([[x, '01'] for x in range(chunksize * 3 + 1)] +
+                                  [[x, '02'] for x in range(chunksize * 3 + 1)] +
+                                  [[x, '03']
+                                      for x in range(chunksize * 3 + 1)],
+                                  columns=list(type_def))
+    test_csv_04_df.to_csv(tempdir.joinpath(test_csv_format.format(24)))
+
+    # key2 chunk<1
+    test_csv_01_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(21)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_01_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize - 1
+        # end for
+
+    assert count == 3
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key2 chunk=1
+    test_csv_02_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(22)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_02_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize
+        # end for
+
+    assert count == 3
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key2 chunk>1
+    test_csv_03_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(23)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_03_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize * 2 - 1
+        # end for
+
+    assert count == 3
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key2 chunk>2
+    test_csv_04_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(24)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_04_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize * 3 + 1
+        # end for
+
+    assert count == 3
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key1,2,3 chunk<1
+    test_csv_05_df = pd.DataFrame([[x, '01'] for x in range(chunksize - 2)] +
+                                  [[x, '02'] for x in range(chunksize - 2)] +
+                                  [[x, '03'] for x in range(chunksize - 2)], columns=list(type_def))
+    test_csv_05_df.to_csv(tempdir.joinpath(test_csv_format.format(25)))
+
+    test_csv_05_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(25)),
+        dtype=type_def, chunksize=4)
+
+    my_reader = GroupByChunkReader(test_csv_05_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == chunksize - 2
+        # end for
+
+    assert count == 3
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+
+    # key1 chunk<1, key2 chunk=1, key3 chunk>1
+    test_csv_06_df = pd.DataFrame([[x, '01'] for x in range(chunksize - 2)] +
+                                  [[x, '02'] for x in range(chunksize)] +
+                                  [[x, '03']
+                                      for x in range(chunksize * 10 + 1)],
+                                  columns=list(type_def))
+    test_csv_06_df.to_csv(tempdir.joinpath(test_csv_format.format(26)))
+
+    test_csv_06_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(26)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_06_df, 'key')
+    count = 0
+    expected = {1: chunksize - 2, 2: chunksize, 3: chunksize * 10 + 1}
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == expected[count]
+        # end for
+
+    assert count == 3
+    with pytest.raises(StopIteration):
+        next(my_reader)
+        # end with
+    # end def
+
+
+@pytest.mark.run(order=40)
+def test_random_patterns(tempdir: Path, logger: Logger):
+    logger.info('random_patterns')
+
+    key_size = random.randint(1, 100)
+    source_record = []
+    expected = {}
+    for key in range(key_size):
+        record_size = random.randint(1, 100)
+        expected[key + 1] = record_size
+        source_record = source_record + \
+            [[x, f'{key:0>3}'] for x in range(record_size)]
+        # end for
+
+    # key1 chunk<1
+    test_csv_01_df = pd.DataFrame(source_record, columns=list(type_def))
+    test_csv_01_df.to_csv(tempdir.joinpath(test_csv_format.format(31)))
+
+    # key1 chunk<1
+    test_csv_01_df = pd.read_csv(
+        tempdir.joinpath(
+            test_csv_format.format(31)),
+        dtype=type_def, chunksize=chunksize)
+
+    my_reader = GroupByChunkReader(test_csv_01_df, 'key')
+    count = 0
+    for this_df in my_reader:
+        count += 1
+        assert len(this_df) == expected[count]
+        # end for
+
+    assert count == key_size
     with pytest.raises(StopIteration):
         next(my_reader)
         # end with
